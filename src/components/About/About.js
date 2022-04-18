0b9680e0c74a6ac4b811eda14c6b190ed98a4f2e
himelislam <himel7100@gmail.com>
2022-04-18 19:56:23 +0600

    readme file added

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,16 +2,16 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div style={{minHeight: "400px"}} className='container text-white d-flex my-4 py-4'>
+        <div style={{minHeight: "400px"}} className='container text-white d-flex my-4 py-4 align-items-center gap-4'>
             <div className='col-lg-6'>
-                <h3 className='fw-light'>MESSAGE FROM JODY</h3>
-                <h2>Can People Really Change?</h2>
-                <p className='fw-light mt-3'>At Jody Michael, I believe in you and your capacity to transform your leadership, career and life.</p>
-                <p className='fw-light mt-3'>Here’s what I belive : Easy decisions rarely move the needle. Superficial tactics only drive temporary solutions. Sustainable change requires keen awareness and targeted strategies to build effortless execution over time.</p>
-                <p className='fw-light mt-3'>That’s why coaching can be the catalyst for profound change. Increased performance. Measurable results. Immediate impact.</p>
+                <h3 className='fw-light'>Hi, I'm Muhaiminul Islam Himel</h3>
+                <h2>Goals That I Wanna Achive.</h2>
+                <p className='fw-light mt-3'>I'm on my final year of completing Diploma in Computer Technology from Sylhet Polytechnic Institute.</p>
+                <p className='fw-light mt-3'>I started my web development journey with PHERO web development course and I'm loving it.</p>
+                <p className='fw-light mt-3'>My Next Goals in life to complete the course and spend more time on React. After finishing this course I will start applying for jobs. And have plans to move forward in this programming journey.</p>
             </div>
             <div className='col-lg-6'> 
-                <img className='img-fluid' src="https://www.jodymichael.com/wp-content/uploads/2018/01/JodyThumbnailBlack1.jpg" alt="" />
+                <img className='img-fluid' src="https://media-exp1.licdn.com/dms/image/C5603AQEI8zveUbevvQ/profile-displayphoto-shrink_800_800/0/1646662961493?e=1655942400&v=beta&t=RssUISln0TpICpWNBnbTZKRScsCuoSH4EollT3Zdgfs" alt="" />
             </div>
         </div>
     );
--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -2,8 +2,39 @@ import React from 'react';
 
 const Blogs = () => {
     return (
-        <div style={{minHeight: "400px"}}>
-            <h2 className='text-white text-center'>This is Blog Page</h2>
+        <div className='container' style={{ minHeight: "400px" }}>
+            <div className='border rounded p-4 text-white my-4'>
+                <div>
+                    <h3 className='fw-light'>Q.1: Difference between authorization and authentication</h3>
+                    <p className='fw-light'>Answer:</p>
+                    <div>
+                        <p>Authentication :</p>
+                        <ul>
+                            <li>The identity of users are checked for providing the access to the system.</li>
+                            <li>Users or persons are verified.</li>
+                            <li>It is done before the authorization process.</li>
+                            <li>It needs usually user’s login details.</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <p>Authorization :</p>
+                        <ul>
+                            <li>While in authorization process, person’s or user’s authorities are checked for accessing the resources.</li>
+                            <li>While in this process, users or persons are validated.</li>
+                            <li>While this process is done after the authentication process.</li>
+                            <li>While it needs user’s privilege or security levels.</li>
+                        </ul>
+                    </div>
+                </div>
+                <div>
+                    <h3 className='fw-light'>Q.2: Why are you using firebase? What other options do you have to implement authentication?</h3>
+                    <p className='fw-light'>Answer: Firebase is a Backend-as-a-Service (Baas). Firebase provides varity of tools and services for developers so that they can build quality apps.There are also other options besides firebase. Like Auth0, Okta, OneLogin, SecureAuth, SafeNet etc</p>
+                </div>
+                <div>
+                    <h3 className='fw-light'>Q.3: What other services does firebase provide other than authentication?</h3>
+                    <p className='fw-light'>Answer: Firebase also provides varity of tools besides authentication.Firebase Provides Website hosting, Firebase Database, Realtime Database, Storage, Function, Machine Learning. So that we can host our website in firebase, we can use Cloud database for our project, amd we can also use Firebase storage to store our data.</p>
+                </div>
+            </div>
         </div>
     );
 };
